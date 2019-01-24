39cc771eab3befec6b786536cfaee7a6c5bb4006
Thomas <45467550+thom-s@users.noreply.github.com>
2019-01-24 14:20:33 -0500

    Update remote-bitlocker-encryption-report.ps1
    Added parameter options

--- remote-bitlocker-encryption-report/remote-bitlocker-encryption-report.ps1
+++ remote-bitlocker-encryption-report/remote-bitlocker-encryption-report.ps1
@@ -1,18 +1,32 @@
 # Parameters
-$computers = Get-Content "computers.txt" # The list of computers, one computer name (hostname) per line
-$reportfile = "report.csv"               # File name of the report
+param(
+    [string] $ComputerList = $(Join-Path -Path $PSScriptRoot -ChildPath "computers.txt"), # Path to the list of computers
+    [string] $ReportPath = $(Join-Path -Path $PSScriptRoot -ChildPath "report.csv")       # Path to the report to output
+)
+
+# Constants
+$Computers = Get-Content $ComputerList
 
 # Initialise the report array, including the first line
 $report = @()
 $report += ,@("Hostname","Encryption Method")
 
 # Get the encryption type for each PC
-$computers | ForEach-Object{                                                                        # Loop through each computer
-  $bitlocker_status = manage-bde -status -computername $_ | Where {$_ -match "Encryption Method:"}  # Run manage-bde remotely and match the "Encryption Method" line
-    $bitlocker_status = $bitlocker_status.Replace("Encryption Method:","").Trim()                   # Remove "Encryption Method:" header from the line
-    $report += , @($_,$bitlocker_status)                                                            # Add the PC and Bitlocker encryption type to the report 
+$Computers | ForEach-Object{# Loop through each computer
+    $computer = $_
+    Write-Output "Connecting to $computer ..."
+    Try{                                                                  
+        $bitlocker_status = manage-bde -status -computername $computer | Where {$_ -match "Encryption Method:"}  # Run manage-bde remotely and match the "Encryption Method" line
+        $bitlocker_status = $bitlocker_status.Replace("Encryption Method:","").Trim()                            # Remove "Encryption Method:" header from the line
+        $report += , @($computer,$bitlocker_status)                                                              # Add the PC and Bitlocker encryption type to the report
+        Write-Output "Done."
+    }
+    Catch{                                           # If the computer is not reachable
+        $report += , @($computer,"UNREACHABLE")      # Output UNREACHABLE to the report
+        Write-Output "Couldn't connect to $computer"
+    }                                                             
 }
 
 # Create the CSV report
-Add-Content .\$reportfile -Value "SEP=,"                     # Write out the first line
-$report | % { $_ -join ","} | Out-File .\$reportfile -Append # Write out the rest of the report
+Add-Content $ReportPath -Value "SEP=,"                     # Write out the first line
+$report | % { $_ -join ","} | Out-File $ReportPath -Append # Write out the rest of the report
