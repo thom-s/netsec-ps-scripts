dc2be5442d447b1599355502a63e4c957152cb6b
Thomas <45467550+thom-s@users.noreply.github.com>
2019-02-12 16:07:51 -0500

    Update printer-auth-report.ps1
    Switching from Write-Outpute to Write-Verbose
    Supressed Test-Connection output thanks to https://github.com/PowerShell/PowerShell/issues/6768#issuecomment-390437427

--- printer-auth-report/printer-auth-report.ps1
+++ printer-auth-report/printer-auth-report.ps1
@@ -31,23 +31,71 @@ Try{
     }
 }
 Catch{
-    Write-Output $_
+    Write-Error $_
 }
 
 # Test all printers
 $printers | Foreach-Object{
     
-    $printer_name = $($_[0])
-    $printer_ip = $($_[1])
-    $printer_server = $($_[2])
+    $printer_name, $printer_ip, $printer_server = $_
     $printer_reachable = $null
     $printer_secured = $null
     $return_codes = @()
 
-    Write-Output "`n=== Testing $printer_name at $printer_ip ===`n"
+    Write-Verbose -Verbose -Message "=== Testing $printer_name at $printer_ip ==="
 
-    If(Test-Connection -Computername $printer_ip -Count 1 -Quiet){
-        Write-Output "Reached $printer_name at $printer_ip"
+    If(Test-Connection -Computername $printer_ip -Count 1 -Quiet -InformationAction Ignore ){
+        Write-Verbose -Verbose -Message "Reached $printer_name at $printer_ip"
+        $printer_reachable = $true
+
+        # Test all web paths
+        $TestPaths | ForEach-Object{
+
+
+            $query_url = "$($Protocol)://$printer_ip/$_"
+            Write-Verbose -Verbose -Message "Testing $printer_name at $query_url"
+
+            try {
+                $response = Invoke-WebRequest -URI $query_url -SkipCertificateCheck -MaximumRedirect 0
+                $status_code = [int]$response.StatusCode
+                if($status_code -eq 0){
+                    $status_code = 200
+                }
+            }
+            catch {
+                $status_code = [int]$_.Exception.Response.StatusCode
+            }
+
+            $return_codes += , $status_code 
+            Write-Verbose -Verbose -Message "$query_url returned HTTP $status_code"
+    
+        }
+        
+        If($return_codes.Contains(401)){
+            $printer_secured = 'Secure'
+        }
+        Elseif($return_codes.Contains(200)){
+            $printer_secured = 'Unsecured'
+        }
+        Else{
+            $printer_secured = 'Needs Manual Review'
+        }
+
+    }
+    Else{
+        Write-Verbose -Verbose -Message "Couldn't reach $printer_name at $printer_ip"
+        $printer_reachable = $false
+        $printer_secured = 'Unreachable'
+    }
+
+    $report += ,@($printer_server, $printer_name,$printer_ip, $printer_reachable, $printer_secured)
+}
+
+Write-Verbose -Verbose -Message "=== Generating CSV report to $ReportPath ==="
+# Create the CSV report
+Add-Content $ReportPath -Value "SEP=,"                     # Write out the first line
+$report | % { $_ -join ","} | Out-File $ReportPath -Append # Write out the rest of the report
+Write-Verbose -Verbose -Message "Done!"
         $printer_reachable = $true
 
         # Test all web paths
